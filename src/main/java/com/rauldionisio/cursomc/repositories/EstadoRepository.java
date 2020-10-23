31788b5e39fd62c33513cf46400f0143548f82a1
Raul Dionisio <rauldionisiosh@gmail.com>
2020-10-23 10:59:12 -0300

    Estado e cidade

---
+++ src/main/java/com/rauldionisio/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.rauldionisio.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rauldionisio.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
