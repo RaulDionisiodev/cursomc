5ae67c46438733b6cb1c58f9e88f8680d5b7590f
Raul Dionisio <rauldionisiosh@gmail.com>
2020-11-20 18:17:46 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/rauldionisio/cursomc/services/ClienteService.java
+++ src/main/java/com/rauldionisio/cursomc/services/ClienteService.java
@@ -9,11 +9,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rauldionisio.cursomc.domain.Cidade;
 import com.rauldionisio.cursomc.domain.Cliente;
-import com.rauldionisio.cursomc.domain.Cliente;
+import com.rauldionisio.cursomc.domain.Endereco;
+import com.rauldionisio.cursomc.domain.enuns.TipoCliente;
 import com.rauldionisio.cursomc.dto.ClienteDTO;
+import com.rauldionisio.cursomc.dto.ClienteNewDTO;
+import com.rauldionisio.cursomc.repositories.CidadeRepository;
 import com.rauldionisio.cursomc.repositories.ClienteRepository;
+import com.rauldionisio.cursomc.repositories.EnderecoRepository;
 import com.rauldionisio.cursomc.services.exceptions.DataIntegrityException;
 import com.rauldionisio.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -23,12 +29,26 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id:" + id + ", tipo: " + 
 		Cliente.class.getName()));
 	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	} 
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj =   find(obj.getId());
@@ -58,6 +78,24 @@ public class ClienteService {
 	public Cliente fromDto(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
+	
+	public Cliente fromDto(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				 TipoCliente.toEnun(objDto.getTipo()));
+		Optional<Cidade> cid = cidadeRepository.findById(objDto.getCidadeId());
+		Endereco end = new Endereco(null,objDto.getLogradouro(),objDto.getNumero(),
+				objDto.getComplemento(),objDto.getBairro(), objDto.getCep(), cli, cid.get());
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+	
 	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
