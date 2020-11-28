a4355b1cc2f5da73b26cc8ca02c0074538ff7314
Raul Dionisio <rauldionisiosh@gmail.com>
2020-11-28 16:47:59 -0300

    Validação customizada: Email não repetido na atualização de cliente

--- src/main/java/com/rauldionisio/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rauldionisio/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.rauldionisio.cursomc.domain.Cliente;
+import com.rauldionisio.cursomc.services.Validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L; 
 	
